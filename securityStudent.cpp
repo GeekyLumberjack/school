a964c43d626a6cc4a9242164e8b52c5b14227eb7
GeekyLumberjack <marshall.holmes7@gmail.com>
2018-04-27 21:01:16 -0500

    Create securityStudent.cpp

---
+++ securityStudent.cpp
@@ -0,0 +1,11 @@
+#include "securityStudent.h"
+
+Degree SecurityStudent::getDegreetype()
+{
+	return Degreetype;
+}
+
+void SecurityStudent::setDegreetype(Degree Degreetype)
+{
+	Degreetype = Degreetype;
+}
